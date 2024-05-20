ed18222a64b8ab43709a4a41561e3415be1bed2a
kicsijocika8@gmail.com <kicsijocika8@gmail.com>
2024-05-20 21:47:29 +0200

    Mozgas metodus

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -8,3 +8,35 @@ const char BANDITA = 'b';
 const char MUMIA = 'm';
 const char KOVACS = 'K';
 const char LADA = 'L';
+char[,] terkep = new char[SOR, OSZLOP];
+
+ConsoleKeyInfo key;
+int sor = 1;
+int oszlop = 0;
+
+
+
+
+
+
+
+void Mozgás()
+{
+    key = Console.ReadKey(true);
+    if (key.Key == ConsoleKey.W && terkep[sor - 1, oszlop] != FAL)
+    {
+        sor--;
+    }
+    else if (key.Key == ConsoleKey.S && terkep[sor + 1, oszlop] != FAL)
+    {
+        sor++;
+    }
+    else if (key.Key == ConsoleKey.A && terkep[sor, oszlop - 1] != FAL && terkep[sor, oszlop] != START)
+    {
+        oszlop--;
+    }
+    else if (key.Key == ConsoleKey.D && terkep[sor, oszlop + 1] != FAL)
+    {
+        oszlop++;
+    }
+}
